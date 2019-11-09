a5e2cead2890ff3acd4610525d77624cc6ca22d5
Nicolas fau <nicolasfau93@gmail.com>
2019-11-09 23:48:07 +0100

    [FIX] Now run 2 jars in the same container

--- docker-images/script.sh
+++ docker-images/script.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
-
 #set -m
-
-java -jar /workspace/lib.jar
-
-java -jar /workspace/demo.jar > log.log 2>&1
+java -jar workspace/lib.jar &
+java -jar workspace/demo.jar > workspace/log.log 2>&1
