5db17ac4ea414f3a237883f6c0597492567bb21e
Nicolas fau <nicolasfau93@gmail.com>
2019-11-10 17:53:22 +0100

    [UPDATE] Change field on dataBase to save DockerInstance

--- square/src/main/java/fr/umlv/square/database/logs/LogRessources.java
+++ square/src/main/java/fr/umlv/square/database/logs/LogRessources.java
@@ -1,11 +1,26 @@
 package fr.umlv.square.database.logs;
 
+import java.time.OffsetDateTime;
+import java.util.ArrayList;
+
 import javax.transaction.Transactional;
-import javax.validation.Valid;
+
+import fr.umlv.square.controllers.ApplicationsListRoute;
+import fr.umlv.square.controllers.LogsListRoute;
+import fr.umlv.square.models.LogsApplication;
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
 
 class LogRessources {
 	@Transactional
-	public static void create(@Valid Log log) {
-		log.persist();
+	public static ArrayList<LogsApplication> getByTime(OffsetDateTime time){
+		String queryString = "timestamp > ?1";
+		System.out.println(time);
+		PanacheQuery<Log> query = Log.find(queryString,time);
+		ArrayList<LogsApplication> array= new ArrayList<LogsApplication>(Math.toIntExact(query.count()));
+		query.stream().forEach(e -> {
+			Application a = ApplicationsListRoute.getOneAppRunning(e.dockerInstance);
+			array.add(new LogsApplication(null, e.message,e.timestamp.toString()));
+		});
+		return array;
 	}
 }
