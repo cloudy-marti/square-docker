c72d3892814f005c34c5ca8cd9e1ec6cd4a3991f
Nicolas fau <nicolasfau93@gmail.com>
2019-11-19 01:04:25 +0100

    [ADD] Some functions to persists application

---
+++ square/src/main/java/fr/umlv/square/database/ApplicationRessources.java
@@ -0,0 +1,18 @@
+package fr.umlv.square.database;
+
+import java.util.stream.Stream;
+
+import javax.transaction.Transactional;
+
+import io.quarkus.hibernate.orm.panache.PanacheQuery;
+
+
+class ApplicationRessources {
+	@Transactional
+	@SuppressWarnings("static-access")
+	public static Stream<Application> getApplications(){
+		PanacheQuery<Application> apps = Application.findAll();
+		return apps.stream();
+	}
+
+}
