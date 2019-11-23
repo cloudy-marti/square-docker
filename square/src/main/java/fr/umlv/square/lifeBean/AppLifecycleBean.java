562438a1111607454f7c5523eb8159e17aea7a3f
Nicolas fau <nicolasfau93@gmail.com>
2019-11-23 11:33:20 +0100

    [UPDATE] removing useless import

--- square/src/main/java/fr/umlv/square/lifeBean/AppLifecycleBean.java
+++ square/src/main/java/fr/umlv/square/lifeBean/AppLifecycleBean.java
@@ -1,12 +1,9 @@
 package fr.umlv.square.lifeBean;
 
-
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 import javax.transaction.Transactional;
-
-import fr.umlv.square.database.Application;
 import fr.umlv.square.models.ApplicationsList;
 import io.quarkus.runtime.StartupEvent;
 
