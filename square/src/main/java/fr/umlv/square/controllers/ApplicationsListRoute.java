46a8e6b23f92ac3952bc836e2acbd3605dc9cc9e
Nicolas fau <nicolasfau93@gmail.com>
2019-11-23 11:31:14 +0100

    [UPDATE] Splitting of methods

--- square/src/main/java/fr/umlv/square/controllers/ApplicationsListRoute.java
+++ square/src/main/java/fr/umlv/square/controllers/ApplicationsListRoute.java
@@ -1,4 +1,5 @@
 package fr.umlv.square.controllers;
+
 import java.io.IOException;
 
 import java.util.*;
@@ -26,34 +27,30 @@ import fr.umlv.square.models.ApplicationsList;
 
 import static fr.umlv.square.docker.DockerDeploy.*;
 
-
 @Path("/app")
 public class ApplicationsListRoute {
 
 	private final ApplicationsList appList;
 	private final String port;
-	private final String host;	
-	
+	private final String host;
+
 	@Inject
-	public ApplicationsListRoute(
-		@ConfigProperty(name = "quarkus.http.port") String port,
-		@ConfigProperty(name = "quarkus.http.host") String host,
-		ApplicationsList appList)
-	{
+	public ApplicationsListRoute(@ConfigProperty(name = "quarkus.http.port") String port,
+			@ConfigProperty(name = "quarkus.http.host") String host, ApplicationsList appList) {
 		this.host = host;
 		this.port = port;
 		this.appList = appList;
-		
+
 	}
 
 	@Path("/list")
-	@GET	
+	@GET
 	@Transactional
 	@Produces(MediaType.APPLICATION_JSON)
 	public String list() {
-		this.appList.wrapperInit();
+		this.check_init();
 		StringBuilder str = new StringBuilder();
-		for(var elem : this.appList.getList())
+		for (var elem : this.appList.getList())
 			str.append(Application.serialize(elem));
 		return str.toString();
 	}
@@ -61,78 +58,72 @@ public class ApplicationsListRoute {
 	@Transactional
 	@Path("/deploy")
 	@POST
-	@Produces({MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN})
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN })
 	@Consumes(MediaType.APPLICATION_JSON)
-    public Response deploy(JsonObject obj) {
+	public Response deploy(JsonObject obj) {
 		Objects.requireNonNull(obj);
-		this.appList.wrapperInit();
-		Application app;
+		this.check_init();
 		try {
-			String[] array = getFromJson(obj, "app");
-			
-			if(!this.appList.appAvailable().contains(array[0]))
-				return Response.status(Status.NOT_ACCEPTABLE).entity("Application doesn't exists").build();
-			
-			app = new Application(
-					this.appList.getCount(),
-					array[0],
-					Integer.parseInt(array[1]),
-					getUnboundedLocalPort(),
-					array[0]+"-"+ (this.appList.getDeployID(array[0])));
-
-			if(!deployDocker(app, this.port, this.host))
-				return Response.status(Status.INTERNAL_SERVER_ERROR).build();
-			this.appList.add(app, array[0]);
-			app.addInBDD();
-
-			getRunningInstancesNames();
-
-		} catch(NullPointerException | IndexOutOfBoundsException | NumberFormatException e) {
+			return this.deployApp(obj);
+		} catch (NullPointerException | IndexOutOfBoundsException | NumberFormatException e) {
 			return Response.status(Status.NOT_ACCEPTABLE).entity("Error with the JSON").build();
-		}catch (IllegalStateException e) {
+		} catch (IllegalStateException e) {
 			return Response.status(Status.INTERNAL_SERVER_ERROR).entity("Unbounded port not found").build();
 		} catch (IOException e) {
 			return Response.status(Status.INTERNAL_SERVER_ERROR).entity("IO Error").build();
 		}
+	}
+
+	private Response deployApp(JsonObject obj) throws IOException {
+		String[] array = getFromJson(obj, "app");
+		Application app;
+		if (!this.appList.appAvailable().contains(array[0]))
+			return Response.status(Status.NOT_ACCEPTABLE).entity("Application doesn't exists").build();
+		app = new Application(this.appList.getCount(), array[0], Integer.parseInt(array[1]), getUnboundedLocalPort(),
+				array[0] + "-" + (this.appList.getDeployID(array[0])));
+		if (!deployDocker(app, this.port, this.host))
+			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+		this.appList.add(app, array[0]);
+		app.addInBDD();
+		getRunningInstancesNames();
 		return Response.status(Status.CREATED).entity(Application.serialize(app)).build();
-    }
+	}
 
 	@Transactional
 	@Path("/stop")
 	@POST
-	@Produces({MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN})
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.TEXT_PLAIN })
 	@Consumes(MediaType.APPLICATION_JSON)
-    public Response stop(JsonObject obj) {
+	public Response stop(JsonObject obj) {
 		Objects.requireNonNull(obj);
-		this.appList.wrapperInit();
-		Stop stopVal;
+		this.check_init();
 		try {
-			String[] array = getFromJson(obj, "id");
-			int id = Integer.parseInt(array[0]);
-
-			Optional<Application> tmp = appList.getAppById(id);
-			if(tmp.isEmpty()) {
-				return Response.status(Status.NOT_ACCEPTABLE).entity("Container is no longer listed").build();
-			}
-
-			Application tmpApp = tmp.get();
-			if(!stopDockerInstance(tmpApp.getDockerInst())) {
-				return Response.status(Status.INTERNAL_SERVER_ERROR).build();
-			}
-			this.appList.deleteApp(tmpApp);
-			String elapsedTime = getElapsedTime(tmpApp.getStartTime(), System.currentTimeMillis());
-			stopVal = new Stop(tmpApp, elapsedTime);
-
-			tmpApp.setElapsedTime(elapsedTime);
+			return this.stopApp(obj);
 		} catch (NullPointerException e) {
 			return Response.status(Status.NOT_ACCEPTABLE).entity("Container is no longer listed").build();
-		} catch(IndexOutOfBoundsException | NumberFormatException e) {
+		} catch (IndexOutOfBoundsException | NumberFormatException e) {
 			return Response.status(Status.NOT_ACCEPTABLE).entity("Error with the JSON").build();
 		} catch (IOException e) {
 			return Response.status(Status.INTERNAL_SERVER_ERROR).entity("IO Error").build();
 		}
-         return Response.status(Status.OK).entity(Stop.serialize(stopVal)).build();
-    }
+	}
+
+	private Response stopApp(JsonObject obj) throws IOException {
+		Stop stopVal;
+		String[] array = getFromJson(obj, "id");
+		Optional<Application> tmp = appList.getAppById(Integer.parseInt(array[0]));
+		if (tmp.isEmpty()) {
+			return Response.status(Status.NOT_ACCEPTABLE).entity("Container is no longer listed").build();
+		}
+		Application tmpApp = tmp.get();
+		if (!stopDockerInstance(tmpApp.getDockerInst())) {
+			return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+		}
+		this.appList.deleteApp(tmpApp);
+		String elapsedTime = getElapsedTime(tmpApp.getStartTime(), System.currentTimeMillis());
+		tmpApp.setElapsedTime(elapsedTime);
+		return Response.status(Status.OK).entity(Stop.serialize(new Stop(tmpApp, elapsedTime))).build();
+	}
 
 	private static String[] getFromJson(JsonObject obj, String key) {
 		String str = obj.get(key).toString();
@@ -140,15 +131,16 @@ public class ApplicationsListRoute {
 		return str.split(":");
 	}
 
+	private void check_init() {
+		this.appList.waitStillInit();
+	}
+
 	private static String getElapsedTime(long startTime, long endTime) {
 		long elapsedTime = endTime - startTime;
-
 		String timeTemplate = "%sm%ss";
-
-		long seconds = elapsedTime/1000;
-		long minutes = seconds/60;
-		seconds = seconds - (minutes*60);
-
+		long seconds = elapsedTime / 1000;
+		long minutes = seconds / 60;
+		seconds = seconds - (minutes * 60);
 		return String.format(timeTemplate, minutes, seconds);
 	}
 }
