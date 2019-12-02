02c1758143be368c048ebea83f3276efc21746af
Nicolas fau <nicolasfau93@gmail.com>
2019-12-02 09:58:40 +0100

    [UPDATE] Remove useless import

--- lib-client/lib_cliente/src/main/java/lib_cliente/fr/umlv/LogReader.java
+++ lib-client/lib_cliente/src/main/java/lib_cliente/fr/umlv/LogReader.java
@@ -2,17 +2,14 @@ package lib_cliente.fr.umlv;
 
 import java.io.IOException;
 import java.io.InputStream;
-import java.lang.reflect.UndeclaredThrowableException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpRequest.BodyPublishers;
-import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
 import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.Instant;
-import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
